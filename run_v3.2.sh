ac274f13e648003fb67593f95cbfc05b105c1f9e
RyotaroOkabe <rytrokb@gmail.com>
2023-04-09 15:40:12 -0400

    struct vis

--- run_v3.2.sh
+++ run_v3.2.sh
@@ -5,4 +5,4 @@
 #SBATCH --time=3-23:00			# time (DD-HH:MM)
 #SBATCH --output="slurm/%x_%j.out"		# output file where all text printed to terminal will be stored
 					# current format is set to "job-name_jobID.out"
-nice python cdvae/run_sgo_v1.py data=mp_20 expname=mp_20_sgo model.predict_property=True
+nice python cdvae/run_sgo_v1.py data=mp_20 expname=mp_20_sgo64 model.predict_property=True
