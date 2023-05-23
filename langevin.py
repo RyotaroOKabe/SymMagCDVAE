e02017d631f6be129004477d284b3accc1801cd1
RyotaroOkabe <rytrokb@gmail.com>
2023-05-23 19:34:10 -0400

    langevin.py

--- langevin.py
+++ langevin.py
@@ -12,7 +12,7 @@ from pymatgen.symmetry.analyzer import *
 from pymatgen.analysis.structure_matcher import StructureMatcher
 from pymatgen.symmetry.groups import SpaceGroup
 from cdvae.common.data_utils import lattice_params_to_matrix_torch, frac_to_cart_coords, cart_to_frac_coords
-from cdvae.pl_modules.model import CDVAE
+from cdvae.pl_modules.model_dldx import CDVAE_SGO
 from torch_geometric.data import Data
 from torch.utils.data import Dataset
 import math as m
@@ -152,105 +152,7 @@ def gen(model, test_loader, cfg):
         'time': time.time() - start_time
     }, model_path / gen_out_name)
 
-
-#%%
 # defineLangevin class with dynamics indidivually
-class CDVAE_SGO(CDVAE):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        
-       
-    @torch.no_grad()
-    def langevin_dynamics_sgo(self, z, ld_kwargs, sgo, alpha=1, gt_num_atoms=None, gt_atom_types=None):   #!! play around
-        """
-        decode crystral structure from latent embeddings.
-        z: latent space
-        ld_kwargs: args for doing annealed langevin dynamics sampling:
-            n_step_each:  number of steps for each sigma level.
-            step_lr:      step size param.
-            min_sigma:    minimum sigma to use in annealed langevin dynamics.
-            save_traj:    if <True>, save the entire LD trajectory.
-            disable_bar:  disable the progress bar of langevin dynamics.
-        gt_num_atoms: if not <None>, use the ground truth number of atoms.
-        gt_atom_types: if not <None>, use the ground truth atom types.
-        """
-        if ld_kwargs.save_traj:
-            all_frac_coords = []
-            all_pred_cart_coord_diff = []
-            all_noise_cart = []
-            all_atom_types = []
-
-        # obtain key stats. #OK
-        num_atoms, _, lengths, angles, composition_per_atom = self.decode_stats(
-            z, gt_num_atoms)  #OK #?esgo
-        if gt_num_atoms is not None:
-            num_atoms = gt_num_atoms
-
-        # obtain atom types.    #OK
-        composition_per_atom = F.softmax(composition_per_atom, dim=-1)
-        if gt_atom_types is None:
-            cur_atom_types = self.sample_composition(
-                composition_per_atom, num_atoms)
-        else:
-            cur_atom_types = gt_atom_types
-
-        # init coords.  #OK
-        cur_frac_coords = torch.rand((num_atoms.sum(), 3), device=z.device)
-
-        # annealed langevin dynamics.   
-        for sigma in tqdm(self.sigmas, total=self.sigmas.size(0), disable=ld_kwargs.disable_bar):
-            if sigma < ld_kwargs.min_sigma:
-                break
-            step_size = ld_kwargs.step_lr * (sigma / self.sigmas[-1]) ** 2
-
-            for step in range(ld_kwargs.n_step_each):
-                noise_cart = torch.randn_like(
-                    cur_frac_coords) * torch.sqrt(step_size * 2)
-                pred_cart_coord_diff, pred_atom_types = self.decoder(
-                    z, cur_frac_coords, cur_atom_types, num_atoms, lengths, angles) #? esgo 
-                cur_cart_coords = frac_to_cart_coords(
-                    cur_frac_coords, lengths, angles, num_atoms)
-                pred_cart_coord_diff = pred_cart_coord_diff / sigma
-                if alpha > 0:
-                    frac = cur_frac_coords.copy()
-                    sgo_loss = sgo_cum_loss(cur_frac_coords, sgo, r_max=0.8)    #!
-                    sgo_loss.backward()
-                    dLdx = alpha * frac.grad
-                else: 
-                    dLdx = 0    #!
-                cur_cart_coords = cur_cart_coords + step_size * pred_cart_coord_diff + noise_cart + dLdx#!
-                cur_frac_coords = cart_to_frac_coords(
-                    cur_cart_coords, lengths, angles, num_atoms)
-
-                if gt_atom_types is None:
-                    cur_atom_types = torch.argmax(pred_atom_types, dim=1) + 1
-
-                if ld_kwargs.save_traj:
-                    all_frac_coords.append(cur_frac_coords)
-                    all_pred_cart_coord_diff.append(
-                        step_size * pred_cart_coord_diff)
-                    all_noise_cart.append(noise_cart)
-                    all_atom_types.append(cur_atom_types)
-
-        output_dict = {'num_atoms': num_atoms, 'lengths': lengths, 'angles': angles,
-                       'frac_coords': cur_frac_coords, 'atom_types': cur_atom_types,
-                       'is_traj': False}
-
-        if ld_kwargs.save_traj: #OK
-            output_dict.update(dict(
-                all_frac_coords=torch.stack(all_frac_coords, dim=0),
-                all_atom_types=torch.stack(all_atom_types, dim=0),
-                all_pred_cart_coord_diff=torch.stack(
-                    all_pred_cart_coord_diff, dim=0),
-                all_noise_cart=torch.stack(all_noise_cart, dim=0),
-                is_traj=True))
-
-        return output_dict
-    
-     
-    def forward(self, batch, teacher_forcing, training):
-        pass
-
 
 def reconstructon(loader, model, ld_kwargs, num_evals,
                   force_num_atoms=False, force_atom_types=False, down_sample_traj_step=1):
