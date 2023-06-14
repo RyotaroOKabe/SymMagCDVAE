6d3227b3659343ee73be6ef30b4404cda7b7a87e
RyotaroOkabe <rytrokb@gmail.com>
2023-06-13 21:15:24 -0400

    comp

--- langevin_v1.2.sh
+++ langevin_v1.2.sh
@@ -5,4 +5,4 @@
 #SBATCH --time=3-23:00			# time (DD-HH:MM)
 #SBATCH --output="slurm/%x_%j.out"		# output file where all text printed to terminal will be stored
 					# current format is set to "job-name_jobID.out"
-nice python langevin.py --model_path /home/rokabe/data2/generative/hydra/singlerun/2023-05-18/mp_20_1 --tasks gen --sg 1 --label n1a100 --alpha 100 --save_traj True
+nice python langevin.py --model_path /home/rokabe/data2/generative/hydra/singlerun/2023-05-18/mp_20_1 --tasks gen --sg 191 --label n1a100 --alpha 100 --save_traj True
